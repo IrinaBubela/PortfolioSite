431e9e58209a2017d16d9eb9e7e6023eb2a34618
Irina <bubelairene@gmail.com>
2020-12-13 18:43:19 +0100

    Updated cv

--- src/layouts/Navbar.js
+++ src/layouts/Navbar.js
@@ -76,7 +76,7 @@ class NavbarComponent extends React.Component {
                             <Nav.Link className="nav-item" href="/About">About</Nav.Link>
                             {/* <Nav.Link className="nav-item" href="/Contact">Contact</Nav.Link> */}
                         </Nav>
-                        <a className="btn font-weight-500 ml-lg-4 btn-teal" href="assets/img/BubelaIrinaCV.pdf" download>
+                        <a className="btn font-weight-500 ml-lg-4 btn-teal" href="assets/img/CV-BUBELA.pdf" download>
                             Resume
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-arrow-right ml-2"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
                         </a>
