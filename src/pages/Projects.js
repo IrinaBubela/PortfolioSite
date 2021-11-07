fe5aaeecaf90ac67543f28c7c42c37b40b1bd6f6
Iryna Bubela <i.bubela@kjosk.pl>
2021-11-07 22:05:31 +0100

    Changed link

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -50,7 +50,7 @@ function Projects() {
                                 <div className="col-md-6 mb-4">
                                     <h6>Check the Project</h6>
                                     <p className="mb-2 small">Use that link to explore Demo on Github Pages</p>
-                                    <a className="small text-arrow-icon" href="https://irinabubela.github.io/photography_website_portfolio/">Explore Demo<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></a>
+                                    <a className="small text-arrow-icon" href="https://katyapolyuh.herokuapp.com/">Explore Demo<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></a>
                                 </div>
                                 <div className="col-md-6 mb-4">
                                     <h6>Check the code</h6>
